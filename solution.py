627dc48454b74fc9da02bfc6ec1ae2636f8da6de
Pranay Joshi <joshi.pranay@gmail.com>
2017-01-28 18:37:08 -0500

    solve diagonal sudoku

--- solution.py
+++ solution.py
@@ -28,12 +28,20 @@ def cross(a, b):
     "Cross product of elements in A and elements in B."
     return [s+t for s in a for t in b]
 
+def zip_strings(a, b):
+    "Combine the array of strings into one array with respective elements merged."
+    combined = list(a)
+    for i in range(len(a)):
+        combined[i] = a[i] + b[i]
+    return combined
+
 boxes = cross(rows, cols)
 
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-unitlist = row_units + column_units + square_units
+diagonal_units = [zip_strings(rows, cols), zip_strings(rows, cols[::-1])]
+unitlist = row_units + column_units + square_units + diagonal_units
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 
